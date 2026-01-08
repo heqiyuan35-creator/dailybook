a4192453fe34f06b853cc9eac4faaa9f63c3859a
何启源02 <14071045+he-qiyuan-02@user.noreply.gitee.com>
2026-01-08 11:01:14 +0800

    我对关于页面添加了个点击事件

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="change">点击</button>
   </div>
 </template>
+<script setup>
+import { ref } from 'vue';
 
+  let n=ref(10)
+  function change(){
+    n++
+  }
+
+</script>
 <style>
 @media (min-width: 1024px) {
   .about {
